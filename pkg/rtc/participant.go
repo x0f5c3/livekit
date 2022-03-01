828928dabf2356f7592535b01491617c7a2b8bd8
David Zhao <david@davidzhao.com>
2022-03-01 09:36:11 -0800

    Fix participant update version generation. (#477)
    Previously it was stuck at 1

--- pkg/rtc/participant.go
+++ pkg/rtc/participant.go
@@ -107,7 +107,7 @@ type ParticipantImpl struct {
 	lock       sync.RWMutex
 	once       sync.Once
 	updateLock sync.Mutex
-	version    uint32
+	version    atomic.Uint32
 
 	// callbacks & handlers
 	onTrackPublished func(types.LocalParticipant, types.MediaTrack)
@@ -135,8 +135,8 @@ func NewParticipant(params ParticipantParams, perms *livekit.ParticipantPermissi
 		disallowedSubscriptions:  make(map[livekit.TrackID]livekit.ParticipantID),
 		connectedAt:              time.Now(),
 		rttUpdatedAt:             time.Now(),
-		version:                  params.InitialVersion,
 	}
+	p.version.Store(params.InitialVersion)
 	p.migrateState.Store(types.MigrateStateInit)
 	p.state.Store(livekit.ParticipantInfo_JOINING)
 	p.SetPermission(perms)
@@ -305,7 +305,7 @@ func (p *ParticipantImpl) ToProto() *livekit.ParticipantInfo {
 		JoinedAt: p.ConnectedAt().Unix(),
 		Hidden:   p.Hidden(),
 		Recorder: p.IsRecorder(),
-		Version:  atomic.NewUint32(p.version).Inc(),
+		Version:  p.version.Inc(),
 	}
 	info.Tracks = p.UpTrackManager.ToProto()
 	if p.params.Grants != nil {
