876d509f51745d813734b3360f5bd54322a51942
cnderrauber <zengjie9004@gmail.com>
2022-03-22 21:01:56 +0800

    Revert "revert network cost change"
    This reverts commit 80903b4c05bf57aad91461f2779aea06281a1bf9.

--- pkg/rtc/participant.go
+++ pkg/rtc/participant.go
@@ -36,6 +36,7 @@ const (
 	rttUpdateInterval = 5 * time.Second
 
 	stateActiveCond           = 3 // reliableDCOpen,lossyDCOpen,PeerConnectionStateConnected
+	initNetWorkCost           = 100
 	disconnectCleanupDuration = 15 * time.Second
 )
 
@@ -128,6 +129,7 @@ type ParticipantImpl struct {
 	onClaimsChanged     func(participant types.LocalParticipant)
 
 	activeCounter atomic.Int32
+	networkCost   int32
 }
 
 func NewParticipant(params ParticipantParams) (*ParticipantImpl, error) {
@@ -149,6 +151,7 @@ func NewParticipant(params ParticipantParams) (*ParticipantImpl, error) {
 		disallowedSubscriptions:  make(map[livekit.TrackID]livekit.ParticipantID),
 		connectedAt:              time.Now(),
 		rttUpdatedAt:             time.Now(),
+		networkCost:              initNetWorkCost,
 	}
 	p.version.Store(params.InitialVersion)
 	p.migrateState.Store(types.MigrateStateInit)
@@ -603,6 +606,9 @@ func (p *ParticipantImpl) SetMigrateState(s types.MigrateState) {
 		p.pendingOffer = nil
 		// in case of migration, subscriber data channel will not fire OnOpen callback
 		p.activeCounter.CAS(0, 2)
+
+		// use lower network cost to make sure migrated's node has higher priority candidate
+		p.networkCost--
 	}
 	p.lock.Unlock()
 	if s == types.MigrateStateComplete {
